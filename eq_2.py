385b734cb956bf88b3a7b9fbeb03f516992ccca3
loclincy10 <60200136+loclincy10@users.noreply.github.com>
2020-02-12 22:51:01 -0600

    Add files via upload

---
+++ eq_2.py
@@ -0,0 +1,54 @@
+import json
+import plotly as py
+import plotly.graph_objs as go
+
+in_file = open('eq_data_1_day_m1.json', 'r')
+out_file = open('readable_eq_data.json', 'w')
+
+eq_data = json.load(in_file)
+
+print(type(eq_data))
+
+json.dump(eq_data,out_file,indent=4)
+
+list_of_eqs = eq_data['features']
+
+print(type(list_of_eqs))
+
+print(len(list_of_eqs))
+
+mags,lons,lats = [],[],[]
+
+for eq in list_of_eqs:
+    mag = eq['properties']['mag'] #that readble file is a list of dictionaries
+    lon = eq['geometry']['coordinates'][0]
+    lat = eq['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+
+from plotly.graph_objs import Scattergeo,Layout
+from plotly import offline as go
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'marker':{
+        'size':[5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale':True,
+        'colorbar':{'title': 'Magnitude'}
+              
+    },
+}]
+
+
+my_layout = Layout(title="Global Earthquakes")
+
+fig = {'data': data, 'layout':my_layout}
+
+go.plot(fig,filename='global_earthquakes.html')
